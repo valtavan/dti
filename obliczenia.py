6a82f9ccccb4b9d962a11d4945d8d18f912557b4
valtavan <stas.nar@gmail.com>
2019-01-16 18:08:08 +0100

    Add files via upload

---
+++ obliczenia.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  1 23:47:26 2019
+
+@author: Stanisław Narębski
+"""
+
+import numpy as np
+from pathlib import Path
+
+p = Path('.')
+
+avg = np.zeros((97,97))
+n   = 0
+
+for path in p.glob('*.txt'):
+    #print(path)
+    curr = np.loadtxt(path)
+    avg += curr
+    n   += 1
+    
+print('{:d} files'.format(n))
+avg = avg/n
+
+np.savetxt('avg.txt', avg)
